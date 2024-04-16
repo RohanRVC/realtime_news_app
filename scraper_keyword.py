ea4ad8b271d6400f80fddeef94e7415803c9f53b
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-04-17 00:04:47 +0530

    Update scraper_keyword.py

--- scraper_keyword.py
+++ scraper_keyword.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd  
 
 url = 'https://edition.cnn.com/'
-r = requests.get(url) 
+r = requests.get(url)  
 html_content = r.text  
 soup = BeautifulSoup(html_content, 'html.parser') 
 # print(soup) 
