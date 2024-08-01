d39d96441a87c2be6d895eb529b9487324b44585
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-08-01 12:43:51 +0530

    Update scraper_top_news.py

--- scraper_top_news.py
+++ scraper_top_news.py
@@ -7,7 +7,7 @@ r = requests.get(url)
 html_content = r.text
 soup = BeautifulSoup(html_content, 'html.parser')  
 # print(soup)  
-# container__headline-text    
+# container__headline-text     
 
 li=[] 
 main_text=''
