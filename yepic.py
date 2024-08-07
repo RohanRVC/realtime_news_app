7fd5946c335bdea5352a8907f83ef12f1f2babc8
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-08-07 12:41:24 +0530

    Update yepic.py

--- yepic.py
+++ yepic.py
@@ -12,7 +12,7 @@ json_data = {
                 {  
                     'type': 'AvatarOverlay', 
                     'assetId': '1a901a33-8783-418e-af6a-a66dae945673', 
-                    'voiceId': 'en-US-JennyMultilingualNeural', 
+                    'voiceId': 'en-US-JennyMultilingualNeural',  
                     'script': "Well done! This is your first video using Yepic's API.", 
                     'xPosition': 448, 
                     'yPosition': 56,
