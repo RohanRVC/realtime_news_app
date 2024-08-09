d04afc46412c54bf6b103931c87d9c8e8455b1a6
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-08-09 13:10:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template,request,abort  
 import requests,time    
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup  
 from random import shuffle           
        
 app = Flask(__name__)     
