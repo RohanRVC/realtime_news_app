21bf9e941e1046cf7ca83aba7ebbae57b157dac8
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-08-25 11:01:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from random import shuffle
 app = Flask(__name__)     
             
     
-# Create a session object          
+# Create a session object           
 session = requests.Session()             
 # Set 'keep_alive' to False          
 session.keep_alive = False         
