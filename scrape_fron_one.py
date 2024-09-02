41bed8af17e7a010a8ff448d68923a3d990f3335
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-09-02 09:21:40 +0530

    Update scrape_fron_one.py

--- scrape_fron_one.py
+++ scrape_fron_one.py
@@ -8,7 +8,7 @@ html_content = r.text
 soup = BeautifulSoup(html_content, 'html.parser')      
 # print(soup)        
 # container__headline-text   
-title_element = soup.find('div', class_="col-sm-8 col-vcenter col-xs-12")         
+title_element = soup.find('div', class_="col-sm-8 col-vcenter col-xs-12")          
          
     # Extract the text from the title element      
 title_text = title_element.get_text(strip=True) if title_element else 'Title not found'      
