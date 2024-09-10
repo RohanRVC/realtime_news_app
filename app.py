c629744abbfde378a30407c9978aa8f8206df3fb
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-09-10 10:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def home():
     html_content = r.text        
     soup = BeautifulSoup(html_content, 'html.parser')      
     
-    ribbon_container_headline = [] 
+    ribbon_container_headline = []  
     for i in soup.find_all('div', class_='container__headline container_ribbon__headline'): 
         if i.get_text().strip() != '':   
             ribbon_container_headline.append(i.get_text().strip()) 
