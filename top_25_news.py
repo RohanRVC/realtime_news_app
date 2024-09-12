9acebeb85aebdb8496aa0ab4de11d21d7af12310
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-09-12 11:38:41 +0530

    Update top_25_news.py

--- top_25_news.py
+++ top_25_news.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 # import pandas as pd
      
-url = 'https://www.prnewswire.com/news-releases/business-technology-latest-news/business-technology-latest-news-list/'  
+url = 'https://www.prnewswire.com/news-releases/business-technology-latest-news/business-technology-latest-news-list/'   
 r = requests.get(url)       
 html_content = r.text       
 soup = BeautifulSoup(html_content, 'html.parser')    
