1d5c61d621ce92268be8d8e46a094086936c7974
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-09-26 11:34:40 +0530

    Update top_25_news.py

--- top_25_news.py
+++ top_25_news.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-# import pandas as pd
+# import pandas as pd 
       
 url = 'https://www.prnewswire.com/news-releases/business-technology-latest-news/business-technology-latest-news-list/'       
 r = requests.get(url)                 
