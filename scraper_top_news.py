516ee69eaa1c27a7e214d5b33617b3661a7476ab
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-02-18 11:20:56 +0530

    Update scraper_top_news.py

--- scraper_top_news.py
+++ scraper_top_news.py
@@ -18,7 +18,7 @@ for i in soup.find_all('a', class_="post-block__title__link"):
     break   
 img_tag = soup.find('img')
 img_link = img_tag['src'] if img_tag else 'No image found' 
-a_tag = soup.find('a', class_='post-block__title__link')
+a_tag = soup.find('a', class_='post-block__title__link') 
 # If there's no class to search by, just find any <a> tag within the <figure>
 if not a_tag:
     figure = soup.find('figure', class_='post-block_media')
