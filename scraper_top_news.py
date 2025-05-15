f51dc50efef0689e01a82db9dc64ff6d69410580
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-05-15 09:28:42 +0530

    Update scraper_top_news.py

--- scraper_top_news.py
+++ scraper_top_news.py
@@ -6,7 +6,7 @@ url = 'https://techcrunch.com/'
 r = requests.get(url)           
 html_content = r.text             
 soup = BeautifulSoup(html_content, 'html.parser')                
-# print(soup)       
+# print(soup)        
 # container__headline-text        
     
 li=[]      
